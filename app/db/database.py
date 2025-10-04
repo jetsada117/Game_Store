297f1bb34355ea5dd67523ecbff2baa1453f2cc3
jetsada117 <jornny.prombut@gmail.com>
2025-10-04 15:35:12 +0700

    delete file render

--- app/db/database.py
+++ app/db/database.py
@@ -9,7 +9,7 @@ load_dotenv()
 MYSQL_USER = os.getenv("MYSQL_USER", "root")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "password")
 MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
-MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
+MYSQL_PORT = os.getenv("MYSQL_PORT", "3309")
 MYSQL_DB = os.getenv("MYSQL_DB", "game_store")
 
 DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e
-alembic upgrade head
-uvicorn app.main:app --host 0.0.0.0 --port "$PORT"
