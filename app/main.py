76ca59c9130ed0fd6f14eee8597a44acd1561995
jetsada117 <jornny.prombut@gmail.com>
2025-10-06 03:43:14 +0700

    login

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from app.controller import auth
 from app.controller.index import router as index_router
 from app.controller.user import router as user_router
 from app.db import models
@@ -10,3 +11,4 @@ app = FastAPI(title="My API", version="1.0.0")
 
 app.include_router(index_router)
 app.include_router(user_router)
+app.include_router(auth.router) 
