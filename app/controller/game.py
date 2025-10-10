0c33e30f38961c61d86fb1eb09f14c88e30d0481
jetsada117 <jornny.prombut@gmail.com>
2025-10-11 01:55:32 +0700

    add status code

--- app/controller/game.py
+++ app/controller/game.py
@@ -1,5 +1,5 @@
 from typing import List
-from fastapi import APIRouter, Depends, File, Form, HTTPException, UploadFile
+from fastapi import APIRouter, Depends, File, Form, HTTPException, UploadFile, status
 from sqlalchemy.orm import Session
 from app.crud import game as crud_game
 from app.db.dependency import get_db
@@ -13,12 +13,12 @@ def get_game_category(db: Session = Depends(get_db)):
     categories = crud_game.get_game_category(db)
     return categories
 
-@router.post("/category", status_code=201)
+@router.post("/category", status_code= status.HTTP_201_CREATED)
 def add_game_category(name: str = Form(...), db: Session = Depends(get_db)):
     new_type = crud_game.create_game_category(db, name)
     return {"message": "เพิ่มประเภทเกมสำเร็จ", "data": new_type}
 
-@router.post("/", status_code=201)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user_with_image(
     name: str = Form(...),
     type_id: int = Form(...),
